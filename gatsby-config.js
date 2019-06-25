a9b6e88ce11fd890d8627b0646017ad2df18dbba
Antonoiu <Madalin.Antonoiu@merz.com>
2019-06-25 00:30:40 -0400

    Lets see what is this plugin doing...

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,5 +74,17 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      // If you want to use styled components, in conjunction to Material-UI, you should:
+      // - Change the injection order
+      // - Add the plugin
+      options: {
+        // stylesProvider: {
+        //   injectFirst: true,
+        // },
+      },
+      // 'gatsby-plugin-styled-components',
+    },
   ],
 }
